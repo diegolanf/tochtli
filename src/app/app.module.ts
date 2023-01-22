604bef7590df8a303993fb8a290afcec328894d0
diegolanf <diego@langarica.me>
2023-01-22 11:59:45 +0100

    Install transloco via ng add @ngneat/transloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    TranslocoRootModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
